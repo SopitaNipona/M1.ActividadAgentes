41d5739c1df9859bc948ac487956449a3ed1fc34
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2025-01-14 10:44:58 -0600

    Se calcula la matriz de probabilidades

--- actividad.py
+++ actividad.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from typing import Tuple
 
+
 DIRECTIONS_TO_DELTAS_DICT: dict = {
     "up": (-1, 0),
     "down": (1, 0),
@@ -98,7 +99,7 @@ class VacuumModel(ap.Model):
         return (1 - np.sum(self.grid) / self.grid.size) * 100
 
 
-# TODO: Deben de calcular probabilidades de cada tipo de agente sobre las siguientes corridas:
+# DONE: Deben de calcular probabilidades de cada tipo de agente sobre las siguientes corridas:
 """
 Corrida A: Corrida de los agentes cuando limpian todas las celdas hasta el 25% del tiempo máximo, no después.
 Corrida B: Corrida de los agentes cuando limpian todas las celdas hasta el 50% del tiempo máximo, no después.
@@ -106,11 +107,48 @@ Corrida C: Corrida de los agentes cuando limpian todas las celdas hasta el 75% d
 Corrida D: Corrida de los agentes cuando limpian todas las celdas hasta el 100% del tiempo máximo.
 """
 
+probability_matrix: np.ndarray = np.zeros((5, 4))
+number_of_experiments0: int = 20
+for i in range(1):  # TODO: Change to 5
+    for j in range(4):
+
+        params0: dict = {
+            "n": 50,
+            "m": 50,
+            "k0": 10 if i == 0 else 0,
+            "k1": 10 if i == 1 else 0,
+            "k2": 10 if i == 2 else 0,
+            "k3": 10 if i == 3 else 0,
+            "k4": 10 if i == 4 else 0,
+            "dirt_percentage": 0.25,
+            "steps": 20000 * (j + 1) // 4,  # percentage of t_max
+        }
+        sample0: ap.Sample = ap.Sample(params0)
+        experiment0: ap.Experiment = ap.Experiment(
+            VacuumModel, sample0, iterations=number_of_experiments0, record=True
+        )
+        results0: ap.datadict.DataDict = experiment0.run()
+        results_df0: pd.DataFrame = pd.DataFrame(results0.reporters)
+        probability_matrix[i, j] = (
+            results_df0["clean_percentage"].eq(100).sum() / number_of_experiments0
+        )
+
+print(probability_matrix)
+sns.heatmap(probability_matrix, annot=True, fmt=".2f")
+plt.title(
+    "Probability of Success (100% Cleaned) for Different Kinds of Agents and Runs"
+)
+plt.xlabel("Run")
+plt.ylabel("Agent")
+plt.show()
+
+
 # TODO: Deben calcular el agente óptimo, o en su caso, el agente con mayor probabilidad de éxito.
 
-# DONE: Analiza cómo la cantidad de agentes impacta el tiempo dedicado, así como la cantidad de movimientos realizados. 
 
-params: dict = {
+# DONE: Analiza cómo la cantidad de agentes impacta el tiempo dedicado, así como la cantidad de movimientos realizados.
+
+params1: dict = {
     "n": 100,
     "m": 100,
     "k0": ap.IntRange(1, 100),
@@ -122,20 +160,20 @@ params: dict = {
     "steps": 100000,  # t_max
 }
 
-sample: ap.Sample = ap.Sample(params, n=20)
-experiment: ap.Experiment = ap.Experiment(
-    VacuumModel, sample, iterations=1, record=True
+sample1: ap.Sample = ap.Sample(params1, n=20)
+experiment1: ap.Experiment = ap.Experiment(
+    VacuumModel, sample1, iterations=1, record=True
 )
-results: ap.datadict.DataDict = experiment.run()
-results_df: pd.DataFrame = pd.DataFrame(results.reporters)
+results1: ap.datadict.DataDict = experiment1.run()
+results_df1: pd.DataFrame = pd.DataFrame(results1.reporters)
 
-sns.scatterplot(data=results_df, x="k", y="movements_made")
+sns.scatterplot(data=results_df1, x="k", y="movements_made")
 plt.title("Number of Agents vs. Movements Made")
 plt.xlabel("Number of Agents")
 plt.ylabel("Movements Made")
 plt.show()
 
-sns.scatterplot(data=results_df, x="k", y="time_used")
+sns.scatterplot(data=results_df1, x="k", y="time_used")
 plt.title("Number of Agents vs. Time Used")
 plt.xlabel("Number of Agents")
 plt.ylabel("Time Used")
